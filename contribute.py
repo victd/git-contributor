6e1b14af5a91e8c506735c2ef2e9e6b559185db0
victd <55512729+victd@users.noreply.github.com>
2023-10-25 14:43:48 -0400

    Update contribute.py

--- contribute.py
+++ contribute.py
@@ -11,7 +11,7 @@ NUM = 366
 
 # check that you have access to all the libraries
 # is a full schedule Mon-Fri for calendars
-# Ledbury, rep, CanPost
+# Ledbury, Otter
 # courses on pluralsight
 
 def main(def_args=sys.argv[1:]):
