babaa9a3d01e31b9d0ab8dc0e35ac15f970cbb0f
victd <55512729+victd@users.noreply.github.com>
2024-01-05 14:22:09 -0500

    Update test_contribute.py

--- test_contribute.py
+++ test_contribute.py
@@ -3,7 +3,7 @@ import contribute
 from subprocess import check_output
 
 # unit testing Feb 14 2022
-# gobble gobble Oct 7 2022
+# gobble gobble Oct 7 2022 check with the parents
 # test contributions Feb 24 2023, and upload segments 
 # test run Sep 8 2023 
 
